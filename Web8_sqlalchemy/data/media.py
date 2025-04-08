5b9a82abfee41c899c544645b66621eff4d9d875
aleksandranikonova27 <123883369+aleksandranikonova27@users.noreply.github.com>
2025-04-08 14:28:43 +0300

    bd

---
+++ Web8_sqlalchemy/data/media.py
@@ -0,0 +1,20 @@
+# Класс с описанием таблицы медиа записей
+
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Media(SqlAlchemyBase):
+    __tablename__ = 'media'
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    fname = sqlalchemy.Column(sqlalchemy.String,nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+    #type_of_event
+    admin_id = sqlalchemy.Column(sqlalchemy.Integer,sqlalchemy.ForeignKey("admins.id"))
+    admin = orm.relationship('Admin')
---
+++ Web8_sqlalchemy/data/type_of_events.py
@@ -0,0 +1,12 @@
+# Класс с описанием таблицы мероприятий
+
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class TypeOfEvents(SqlAlchemyBase):
+    __tablename__ = 'type_of_events'
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    events = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
